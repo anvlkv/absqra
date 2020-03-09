34f9fb1834794226ebb00e6806baed67b7e0e76b
a.nvlkv <a.nvlkv@gmail.com>
2020-03-09 20:18:44 +0100

    lexer and cursor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::env;
+// extern crate ra;
+use ra_lexer;
+// use ra_parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let path = Path::new(&args[1]);
+    // println!("{:?}", &path);
+    let mut file = File::open(path).unwrap();
+    
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    // let bts:() = file.bytes();
+
+    let mut prog = ra_lexer::tokenize(&contents);
+    
+    println!("{:?}", prog.next());
+    println!("{:?}", prog.next());
+    println!("{:?}", prog.next());
+    println!("{:?}", prog.next());
 }
