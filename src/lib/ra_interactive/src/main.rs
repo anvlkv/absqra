d351ad38281325252b37a6c28f313210e364ef3d
a.nvlkv <a.nvlkv@gmail.com>
2020-04-06 18:16:00 +0200

    add interactive terminal

---
+++ src/lib/ra_interactive/src/main.rs
@@ -0,0 +1,25 @@
+use ra_parser::parse;
+use std::panic;
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    loop {
+        print!("ra_ ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        
+
+        if input.trim().len() > 0 {
+            let result = panic::catch_unwind(|| parse(&input));
+    
+            match result {
+                Ok(r) => println!("{:?}", r),
+                Err(e) => println!("{:?}", e)
+            }
+        }
+
+    }
+}
