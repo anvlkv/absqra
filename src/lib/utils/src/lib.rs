9aaa1ecc0f29323df74b4bf661533aa8f92d01b4
a.nvlkv <a.nvlkv@gmail.com>
2020-06-05 15:50:10 +0200

    dir flat mapper

---
+++ src/lib/utils/src/lib.rs
@@ -0,0 +1,13 @@
+use std::path::Path;
+use std::fs::{DirEntry, read_dir};
+
+pub fn files_from_dir_recursively(path: &str) -> Vec<DirEntry> {
+    println!("{}", path);
+    read_dir(Path::new(path)).unwrap().map(|f| f.unwrap()).flat_map(|entry| {
+        match entry.metadata().unwrap() {
+            meta if meta.is_dir() => files_from_dir_recursively(&entry.path().into_os_string().into_string().unwrap()),
+            meta if meta.is_file() => vec![entry],
+            _ => panic!("unsupported metadata")
+        }
+    }).collect()
+}
