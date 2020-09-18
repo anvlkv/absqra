356f8ee53534c0d2b718c45b708936e92fe7de48
a.nvlkv <a.nvlkv@gmail.com>
2020-09-19 00:14:41 +0200

    ooh it loops bad

--- src/lib/ra_parser/src/expressions/logic_expression.rs
+++ src/lib/ra_parser/src/expressions/logic_expression.rs
@@ -7,12 +7,12 @@ use super::logic_operation::LogicOperation;
 
 
 #[derive(Serialize, Clone, Debug)]
-pub struct LogicExpression<'a> (RaToken<'a>, Option<LogicOperation>, Option<Box<OutputExpression<'a>>>);
+pub struct LogicExpression<'a> (Option<Box<OutputExpression<'a>>>, Option<LogicOperation>, Option<Box<OutputExpression<'a>>>);
 
 impl<'a> ParsedByToken<'a, LogicExpression<'a>> for LogicExpression<'a> {
     fn new(token: RaToken<'a>) -> Result<Box<LogicExpression<'a>>, Vec<ParserError>> { 
         if Self::starts_with_tokens().contains(&token.kind) {
-            Ok(Box::new(Self(token, None, None)))
+            Ok(Box::new(Self(Some(OutputExpression::new(token)?), None, None)))
         }
         else {
             Err(vec![
@@ -26,53 +26,14 @@ impl<'a> ParsedByToken<'a, LogicExpression<'a>> for LogicExpression<'a> {
         }
     }
     fn append_token(self, token: RaToken<'a>) -> Result<Box<LogicExpression<'a>>, Vec<ParserError>> { 
-        let LogicExpression(first_token, op, next) = self.clone();
+        let LogicExpression(left, op, next) = self.clone();
 
-        if op.is_none() && next.is_none() {
-            Ok(Box::new(
-                Self(first_token, Some(*LogicOperation::new(token)?), None)
-            ))
-        }
-        else if op.is_some() && op.clone().unwrap().allowed_tokens().contains(&token.kind) &&  next.is_none() {
-            Ok(Box::new(
-                Self(first_token, Some(*op.unwrap().append_token(token)?), None)
-            ))
-        }
-        else if op.is_some() && op.clone().unwrap().kind.is_some() {
-            if next.is_some() {
-                Ok(Box::new(
-                    Self(first_token, op, Some(next.unwrap().append_token(token)?))
-                ))
-            }
-            else {
-                Ok(Box::new(
-                    Self(first_token, op, Some(OutputExpression::new(token)?))
-                ))
-            }
-        }
-        else {
-            Err(vec![
-                ParserError::ExpectedAGotB(
-                    format!("{:?}", self.allowed_tokens()),
-                    format!("{:?}", token),
-                    token.position.0,
-                    Backtrace::new()
-                )
-            ])
-        }
+        todo!(" implement me! ");
     }
     fn allowed_tokens(&self) -> Vec<TokenKind<'a>> { 
-        let LogicExpression(_, op, next) = self;
+        let LogicExpression(left, op, next) = self;
 
-        if next.is_some() {
-            next.as_ref().unwrap().allowed_tokens().clone()
-        }
-        else if op.is_some() {
-            op.as_ref().unwrap().allowed_tokens().clone()
-        }
-        else {
-            LogicOperation::starts_with_tokens()
-        }
+        todo!(" implement me! ");
     }
 
     fn starts_with_tokens() -> Vec<TokenKind<'static>> { 
