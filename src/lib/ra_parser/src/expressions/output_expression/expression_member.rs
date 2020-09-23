273c663ff468913a14173363a95d14e4c7a40cee
a.nvlkv <a.nvlkv@gmail.com>
2020-09-23 20:49:37 +0200

    draf parsed by token for expression member

--- src/lib/ra_parser/src/expressions/output_expression/expression_member.rs
+++ src/lib/ra_parser/src/expressions/output_expression/expression_member.rs
@@ -1,8 +1,7 @@
 use super::*;
-use super::{operation_expression, grouping_expression, logic_expression};
-use logic_expression::LogicExpression;
-use grouping_expression::GroupingExpression;
-use operation_expression::MathExpression;
+use super::logic_expression::LogicExpression;
+use super::grouping_expression::GroupingExpression;
+use super::operation_expression::MathExpression;
 
 #[derive(Serialize, Clone, Debug)]
 pub enum ExpressionMember<'a> {
@@ -11,4 +10,14 @@ pub enum ExpressionMember<'a> {
     LogicExpression(LogicExpression<'a>),
     GroupingExpression(GroupingExpression<'a>),
     MathExpression(MathExpression<'a>)
+}
+
+impl<'a> ParsedByToken<'a, ExpressionMember<'a>> for ExpressionMember<'a> {
+    fn new(token: RaToken<'a>) -> Result<Box<ExpressionMember<'a>>, Vec<ParserError>> { todo!() }
+    fn append_token(self, token: RaToken<'a>) -> Result<Box<ExpressionMember<'a>>, Vec<ParserError>> { todo!() }
+    fn allowed_tokens(&self) -> Vec<TokenKind<'a>> { todo!() }
+    fn required_tokens(&self) -> Vec<TokenKind<'a>> { 
+        todo!() 
+    }
+    fn starts_with_tokens() -> Vec<TokenKind<'a>> { todo!() }
 }
