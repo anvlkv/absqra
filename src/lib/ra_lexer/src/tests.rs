11df1f21a7ad3d84d4f7e3e3980aa7554c93a93f
a.nvlkv <a.nvlkv@gmail.com>
2020-10-15 19:47:01 +0200

    move special chars test

--- src/lib/ra_lexer/src/tests.rs
+++ src/lib/ra_lexer/src/tests.rs
@@ -8,3 +8,10 @@ fn it_should_match_snapshots(contents: String, file_name: String) {
     let tokens: Vec<RaToken> = tokenize(&contents).map(|r| r.unwrap()).collect();
     assert_json_snapshot!(file_name, tokens)
 }
+
+#[test]
+fn it_should_tokenize_special_characters() {
+    let contents = String::from("!@#$%^&*()_+=-{}?:;\\/|~");
+    let tokens: Vec<RaToken> = tokenize(&contents).map(|r| r.unwrap()).collect();
+    assert_json_snapshot!("special_characters", tokens)
+}
