0dc84a0d303a14d9356a69d38f109f119fc62175
a.nvlkv <a.nvlkv@gmail.com>
2020-10-23 20:48:21 +0200

    simplify can_parse

--- src/lib/ra_parser/src/expression/annotation.rs
+++ src/lib/ra_parser/src/expression/annotation.rs
@@ -7,14 +7,17 @@ pub struct AnnotationExpression {
 
 impl Expression for AnnotationExpression {
     fn can_parse(tokens: &Vec<RaToken>) -> bool { 
-        tokens.first().is_some() && {
-            let first_token = tokens.first().unwrap();
-            first_token.kind == TokenKind::HashPound
-        } && {
-            let mut odd = tokens.iter().enumerate().filter(|(i, _)| i != &0 && i % 2 == 0);
-            let mut even = tokens.iter().enumerate().filter(|(i, _)| i % 2 != 0);
-            odd.all(|(_, t)| t.kind == TokenKind::Colon) && even.all(|(_, t)| t.kind == TokenKind::Identifier(String::new()))
-        }
+        tokens.first().is_some() && tokens.iter().enumerate().all(|(i, t)| {
+            if i == 0 {
+                t.kind == TokenKind::HashPound
+            }
+            else if i % 2 == 0 {
+                t.kind == TokenKind::Colon
+            }
+            else {
+                t.kind == TokenKind::Identifier(String::default())
+            }
+        })
     }
     fn parse(_: &Vec<RaToken>) -> std::result::Result<Self, Vec<errors::ParserError>> { todo!() }
     fn level(&self) -> u16 { todo!() }
