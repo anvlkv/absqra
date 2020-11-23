a199c2681974becca72f307ecd14abbaabaa88ca
a.nvlkv <a.nvlkv@gmail.com>
2020-11-23 15:40:12 +0100

    probably correct parsing

--- src/lib/ra_tree_parser/src/tree.rs
+++ src/lib/ra_tree_parser/src/tree.rs
@@ -1,25 +1,28 @@
 use super::*;
-use serde_indextree::{SiblingNodes};
-use indextree::{Arena, NodeEdge, Node};
-use serde::{Serialize, Serializer};
-use serde::ser::SerializeStruct;
+use indextree::{Arena, Node, NodeId};
 
 #[derive(Serialize, Debug, Clone, PartialEq)]
 pub enum RaBlock {
+    Root,
     Block,
     Group,
     Token(RaToken),
 }
 
+#[derive(Debug, Clone)]
 pub struct RaTree {
     arena: Arena<RaBlock>,
     awaiting_paired_tokens: Vec<TokenKind>,
+    root_id: NodeId,
 }
 
 impl RaTree {
     pub(crate) fn new() -> Self {
+        let mut arena = Arena::new();
+        let root_id = arena.new_node(RaBlock::Root);
         Self {
-            arena: Arena::new(),
+            arena,
+            root_id,
             awaiting_paired_tokens: Vec::new(),
         }
     }
@@ -27,72 +30,52 @@ impl RaTree {
     pub(crate) fn add_token(&mut self, token: RaToken) {
         let paired = token.pair();
         let tree_iter = self.arena.iter();
-        match tree_iter.last() {
-            Some(last_added_ref) => {
-                let last_added = last_added_ref.clone();
-                let last_child_id = last_added
-                    .last_child()
-                    .unwrap_or(self.arena.get_node_id(last_added_ref).unwrap());
+        let last_added_ref = tree_iter.last().unwrap();
+        let last_added = last_added_ref.clone();
+        let last_child_id = last_added
+            .last_child()
+            .unwrap_or(self.arena.get_node_id(last_added_ref).unwrap());
 
-                let mut inserted_id = self.arena.new_node(RaBlock::Token(token.clone()));
+        let mut inserted_id = self.arena.new_node(RaBlock::Token(token.clone()));
 
-                if let Some(paired) = paired {
-                    self.awaiting_paired_tokens.push(paired);
-                    let group_id = self.arena.new_node(RaBlock::Group);
-                    group_id.append(inserted_id, &mut self.arena);
-                    inserted_id = group_id;
-                }
+        if let Some(paired) = paired {
+            self.awaiting_paired_tokens.push(paired);
+            let group_id = self.arena.new_node(RaBlock::Group);
+            group_id.append(inserted_id, &mut self.arena);
+            inserted_id = group_id;
+        }
 
-                match token {
-                    t if self.is_current(&t, &last_added) => {
-                        last_child_id.insert_after(inserted_id, &mut self.arena);
+        if self.is_current(&token, &last_added) {
+            last_child_id.insert_after(inserted_id, &mut self.arena);
+        } else {
+            let block_id = self.arena.new_node(RaBlock::Block);
+            block_id.append(inserted_id, &mut self.arena);
+
+            if self.is_child(&token, &last_added) {
+                last_child_id.append(block_id, &mut self.arena);
+            } else {
+                let parent_block =
+                    last_child_id
+                        .ancestors(&self.arena)
+                        .skip(1)
+                        .find(|next_parent_id| {
+                            let blk = self.arena.get(*next_parent_id).unwrap();
+                            blk.get() == &RaBlock::Block && self.is_sibling(&token, blk)
+                        });
+                match parent_block {
+                    Some(parent_id) => {
+                        parent_id.insert_after(block_id, &mut self.arena);
                     }
-                    t if self.is_child(&t, &last_added) => {
-                        let block_id = self.arena.new_node(RaBlock::Block);
-                        block_id.append(inserted_id, &mut self.arena);
-                        last_child_id.append(block_id, &mut self.arena);
+                    None => {
+                        self.root_id.append(block_id, &mut self.arena);
                     }
-                    t if self.is_sibling(&t, &last_added) => {
-                        let mut parents_iter = last_child_id.reverse_traverse(&self.arena);
-                        while let Some(edge) = parents_iter.next() {
-                            match edge {
-                                NodeEdge::Start(next_parent_id) => {
-                                    match self.arena.get(next_parent_id).unwrap().get() {
-                                        RaBlock::Block => {
-                                            next_parent_id
-                                                .insert_after(inserted_id, &mut self.arena);
-                                            break;
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-                    _ => {}
                 }
-            }
-            None => {
-                let mut inserted_id = self.arena.new_node(RaBlock::Token(token.clone()));
-
-                if let Some(paired) = paired {
-                    self.awaiting_paired_tokens.push(paired);
-                    let group_id = self.arena.new_node(RaBlock::Group);
-                    group_id.append(inserted_id, &mut self.arena);
-                    inserted_id = group_id;
-                }
-
-                let block_id = self.arena.new_node(RaBlock::Block);
-
-                block_id.append(inserted_id, &mut self.arena);
             }
         }
     }
-    
     fn is_child(&self, token: &RaToken, node: &Node<RaBlock>) -> bool {
         match node.get() {
-            RaBlock::Block | RaBlock::Group => match node.last_child() {
+            RaBlock::Root | RaBlock::Block | RaBlock::Group => match node.last_child() {
                 Some(child_id) => self.is_child(token, self.arena.get(child_id).unwrap()),
                 None => false,
             },
@@ -101,10 +84,9 @@ impl RaTree {
             }
         }
     }
-    
     fn is_sibling(&self, token: &RaToken, node: &Node<RaBlock>) -> bool {
         match node.get() {
-            RaBlock::Block | RaBlock::Group => match node.last_child() {
+            RaBlock::Root | RaBlock::Block | RaBlock::Group => match node.last_child() {
                 Some(child_id) => self.is_sibling(token, self.arena.get(child_id).unwrap()),
                 None => false,
             },
@@ -113,32 +95,15 @@ impl RaTree {
             }
         }
     }
-    
     fn is_current(&self, token: &RaToken, node: &Node<RaBlock>) -> bool {
         match node.get() {
-            RaBlock::Block | RaBlock::Group => match node.last_child() {
+            RaBlock::Root | RaBlock::Block | RaBlock::Group => match node.last_child() {
                 Some(child_id) => self.is_current(token, self.arena.get(child_id).unwrap()),
                 None => false,
             },
             RaBlock::Token(last_token) => {
                 token.level == last_token.level
                     && (token.position.0).line() == (last_token.position.0).line()
-            }
-        }
-    }
-}
-
-impl Serialize for RaTree {
-    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error> where S: Serializer { 
-        let first_node = self.arena.iter().nth(0);
-        match first_node {
-            Some(first_node) => {
-                let serialize_node = SiblingNodes::new(self.arena.get_node_id(first_node).unwrap(), &self.arena);
-                serialize_node.serialize(serializer)
-            },
-            None => {
-                let s = serializer.serialize_struct("", 0)?;
-                s.end()
             }
         }
     }
