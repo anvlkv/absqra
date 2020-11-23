4c599541593ca5bb05da1e2bc99845701eef120d
a.nvlkv <a.nvlkv@gmail.com>
2020-11-23 15:35:31 +0100

    use debug snapshots

--- src/lib/ra_tree_parser/src/tests.rs
+++ src/lib/ra_tree_parser/src/tests.rs
@@ -1,5 +1,6 @@
+
 mod lib {
-    use ra_dev_tools::insta::{assert_json_snapshot, assert_snapshot};    
+    use ra_dev_tools::insta::{assert_snapshot, assert_debug_snapshot};    
     use ra_dev_tools::{make_example_tests};
     use ra_lexer::tokenize;
     use crate::parser::parse;
@@ -19,16 +20,15 @@ mod lib {
                     let formatted_errors: String = {
                         errors.into_iter().map(|e| format!("{}", e)).collect::<Vec<String>>().join("\n")
                     };
-
                     
                     assert_snapshot!(err_messages_file_name, formatted_errors);
-                    assert_json_snapshot!(errors_file_name, parsed);
+                    assert_debug_snapshot!(errors_file_name, parsed);
 
 
                     panic!("failed to parse example {:?}", file_name);
                 }
             }
         };
-        assert_json_snapshot!(file_name, tree)       
+        assert_debug_snapshot!(file_name, tree)       
     }
 }
