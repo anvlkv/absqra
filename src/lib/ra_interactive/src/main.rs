4e618faa37ac19caece1afabf129421bfa188dc1
a.nvlkv <a.nvlkv@protonmail.ch>
2021-01-28 20:06:11 +0100

    fix interactive

--- src/lib/ra_interactive/src/main.rs
+++ src/lib/ra_interactive/src/main.rs
@@ -1,48 +1,45 @@
-// extern crate scrawl;
-// use scrawl::{editor};
-// use ra_lexer::tokenize;
-// use ra_parser::ast::RaAST;
-// use std::panic;
-// use std::convert::TryFrom;
+extern crate scrawl;
+use scrawl::editor;
+use ra_lexer::tokenize;
+use ra_parser::parse;
 
 fn main() {
-    // let mut content = String::from("// enter ra code after this line \n\n\n // keep this line to quit \n ra_quit");
+    let mut content = String::from("// enter ra code");
 
-    // let mut output = String::new();
+    let mut output = String::new();
 
-    // let mut quit = false;
+    let mut quit = false;
 
-    // while !quit {
+    while !quit {
 
-    //     let input = editor::new()
-    //                     .contents(&content)
-    //                     .open()
-    //                     .unwrap();
+        let input = editor::new()
+                        .contents(&content)
+                        .open()
+                        .unwrap();
         
 
-    //     let trimmed_input = &input[..input.find("==================\n\n\n").unwrap_or(input.len())];
+        let trimmed_input = &input[..input.find("==================\n\n\n").unwrap_or(input.len())];
         
         
-    //     if trimmed_input.trim().len() > 0 {
-    //         let tree = parse(tokenize(trimmed_input));
-    //         let result = panic::catch_unwind(|| RaAST::try_from(tree.unwrap()));
+        if trimmed_input.trim().len() > 0 {
+            let result = parse(tokenize(trimmed_input));
     
-    //         output = match result {
-    //             Ok(r) => format!("OUTPUT: {:#?}", r),
-    //             Err(e) => format!("ERROR: {:#?}", e)
-    //         };
-    //     }
+            output = match result {
+                Ok(r) => format!("OUTPUT: {:#?}", r),
+                Err(e) => format!("ERROR: {:#?}", e)
+            };
+        }
 
-    //     content = trimmed_input.to_owned();
+        content = trimmed_input.to_owned();
 
-    //     content.push_str("==================\n\n\n");
-    //     content.push_str("/* OUTPUT *");
-    //     content.push_str(&output);
-    //     content.push_str("* END OF OUTPUT */");
+        content.push_str("==================\n\n\n");
+        content.push_str("/* OUTPUT *");
+        content.push_str(&output);
+        content.push_str("* END OF OUTPUT */");
 
 
-    //     quit = trimmed_input.find("ra_quit").is_some();
-    // }
+        quit = trimmed_input.find("ra_quit").is_some();
+    }
 
-    // println!("{}", output);
+    println!("{}", output);
 }
